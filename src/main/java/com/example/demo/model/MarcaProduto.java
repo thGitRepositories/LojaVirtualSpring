8f025263e9bd296b63016ad8164842443afb1ea4
= <=>
2024-08-27 20:25:53 -0300

    Entity marca_produto

---
+++ src/main/java/com/example/demo/model/MarcaProduto.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class MarcaProduto  implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+}
