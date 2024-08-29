7ddb5e029aab2d1346fb661cb17f64fc9870d46b
thGitRepositories <thiagoemp16@outlook.com>
2024-08-29 12:51:22 -0300

    Create table acesso

--- src/main/java/com/example/demo/model/CategoriaProduto.java
+++ src/main/java/com/example/demo/model/CategoriaProduto.java
@@ -12,45 +12,36 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto",sequenceName = "seq_marca_produto",allocationSize = 1,initialValue = 1)
-public class MarcaProduto  implements Serializable{
+@Table(name= "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto",sequenceName = "seq_categoria_produto",allocationSize = 1,initialValue = 1)
+public class CategoriaProduto implements Serializable{
 
-	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_categoria_produto")
 	private Long id;
-	
 	@Column(nullable = false)
 	private String nomeDesc;
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getNomeDesc() {
 		return nomeDesc;
 	}
-
 	public void setNomeDesc(String nomeDesc) {
 		this.nomeDesc = nomeDesc;
 	}
-
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -59,10 +50,11 @@ public class MarcaProduto  implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		return Objects.equals(id, other.id);
 	}
 	
 	
 	
+
 }
