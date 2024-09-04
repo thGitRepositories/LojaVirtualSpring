04d0c48ebdd2f51ef4123f97e9979a9b747e5d60
= <=>
2024-09-03 21:12:58 -0300

    ADD enums

--- src/main/java/com/example/demo/model/Endereco.java
+++ src/main/java/com/example/demo/model/Endereco.java
@@ -2,10 +2,12 @@ package com.example.demo.model;
 
 import java.io.Serializable;
 
-
+import com.example.demo.enums.TipoEndereco;
 
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -38,6 +40,17 @@ public class Endereco implements Serializable {
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
 
+	@Enumerated(EnumType.STRING)
+	private TipoEndereco tipoEndereco;
+	
+	public void setTipoEndereco(TipoEndereco tipoEndereco) {
+		this.tipoEndereco = tipoEndereco;
+	}
+	
+	public TipoEndereco getTipoEndereco() {
+		return tipoEndereco;
+	}
+	
 	public Long getId() {
 		return id;
 	}
