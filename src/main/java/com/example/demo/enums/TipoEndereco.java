3c63442f3ae8795ae9b7a73e0e1801732a5f084e
= <=>
2024-09-03 21:17:29 -0300

    merge com problema

---
+++ src/main/java/com/example/demo/enums/TipoEndereco.java
@@ -0,0 +1,22 @@
+package com.example.demo.enums;
+
+public enum TipoEndereco {
+	COBRANCA("Cobranca"),
+	ENTREGA("Entrega");
+	
+	private String descricao;
+	
+	private TipoEndereco(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return this.descricao;
+	}
+}
