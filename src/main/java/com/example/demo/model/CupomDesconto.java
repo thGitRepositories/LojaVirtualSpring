006e912e8492ceea1a0498e5466e41e891e0f640
= <=>
2024-09-05 18:45:41 -0300

    Produto

---
+++ src/main/java/com/example/demo/model/CupomDesconto.java
@@ -0,0 +1,80 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "cupom_desconto")
+@SequenceGenerator(sequenceName = "seq_cupom_desconto",name =  "seq_cupom_desconto",allocationSize = 1,initialValue = 1)
+public class CupomDesconto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_cupom_desconto")
+	private Long id;
+
+	private String codeDesc;
+	private BigDecimal valorRealDesc;
+	private BigDecimal valorPorcentDesc;
+	
+	@Temporal(TemporalType.DATE)
+	private Date vtDesc;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCodeDesc() {
+		return codeDesc;
+	}
+	public void setCodeDesc(String codeDesc) {
+		this.codeDesc = codeDesc;
+	}
+	public BigDecimal getValorRealDesc() {
+		return valorRealDesc;
+	}
+	public void setValorRealDesc(BigDecimal valorRealDesc) {
+		this.valorRealDesc = valorRealDesc;
+	}
+	public BigDecimal getValorPorcentDesc() {
+		return valorPorcentDesc;
+	}
+	public void setValorPorcentDesc(BigDecimal valorPorcentDesc) {
+		this.valorPorcentDesc = valorPorcentDesc;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CupomDesconto other = (CupomDesconto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
